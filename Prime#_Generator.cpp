5cb299a87d6ea4b3b9cce7162124ca1a1674588e
vivaan3141 <155184535+vivaan3141@users.noreply.github.com>
2025-11-27 17:37:34 -0800

    Update main.cpp to newer code with all ports and controls added
    Implement a prime number generator that checks and displays prime numbers up to a user-defined limit.

---
+++ Prime#_Generator.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num=0;
+    int c=0;
+    int x=0;
+    
+    cout << "Number: ";
+    cin >> num;
+    if (num<=1){
+        cout<<"None";
+    }
+     
+    
+    
+    
+    
+    for (int j=2; j<=num; j++){
+        c=0;
+        for (int i = 2; i <= (j/2); i++) {
+            if (j % i==0){
+                c=1;
+            }
+        }
+        if (c==0){
+            cout<<j<<" is a prime number!"<<endl;
+        }
+        
+     
+    }
+         cout<<endl;
+
+   
+    
+    
+    
+   
+
+    return 0;
+}
+
+
+
+
+
+
